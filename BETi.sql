50fb4edd1cdd6075fe1595a43bc0d38071737de0
Alex1sK1ng <alexgatabarros@gmail.com>
2020-10-26 11:56:50 +0100

    BETi

--- BETi.sql
+++ BETi.sql
@@ -6,14 +6,14 @@ GO
 
 CREATE TABLE BET_Usuarios (
 
-	--ID INT NOT NULL IDENTITY(1,1),
 	CorreoElectronico VARCHAR(50) NOT NULL,
 	Contraseña VARCHAR(128) NOT NULL,
 	Saldo MONEY NOT NULL,
 	
-	CONSTRAINT PKUsuarios PRIMARY KEY(CorreoElectronico, Contraseña),
+	CONSTRAINT PKUsuarios PRIMARY KEY(CorreoElectronico),
 	CONSTRAINT CKCorreoElectronico CHECK (CorreoElectronico LIKE '%@%.%'),
-	CONSTRAINT CKContraseña CHECK (DATALENGTH(Contraseña) > 7)
+	CONSTRAINT CKContraseña CHECK (DATALENGTH(Contraseña) > 7),
+	CONSTRAINT CKSaldo CHECK (Saldo > 0.0)
 
 )
 GO
@@ -23,6 +23,7 @@ CREATE TABLE BET_Equipos (
 	ID CHAR(3) NOT NULL,
 	Nombre VARCHAR(20) NOT NULL,
 	Ciudad VARCHAR(30) NULL,
+	Pais VARCHAR(30) NULL,
 
 	CONSTRAINT PKEquipos PRIMARY KEY(ID)
 
@@ -41,8 +42,10 @@ CREATE TABLE BET_Partidos (
 	Fecha SMALLDATETIME NULL,
 
 	CONSTRAINT PKPartidos PRIMARY KEY(ID),
-	CONSTRAINT FKPartidosEquipoLocal FOREIGN KEY(IDEquipoLocal) REFERENCES BET_Equipos(ID) ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT FKPartidosEquipoVisitante FOREIGN KEY(IDEquipoVisitante) REFERENCES BET_Equipos(ID) ON DELETE NO ACTION ON UPDATE CASCADE
+	CONSTRAINT FKPartidosEquipoLocal FOREIGN KEY(IDEquipoLocal) REFERENCES BET_Equipos(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT FKPartidosEquipoVisitante FOREIGN KEY(IDEquipoVisitante) REFERENCES BET_Equipos(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT CKGolesLocal CHECK (GolesLocal > 0),
+	CONSTRAINT CKGolesVisitante CHECK (GolesVisitante > 0) 
 
 )
 GO
@@ -56,7 +59,6 @@ CREATE TABLE BET_Apuestas (
 	Acertada BIT NOT NULL,
 	IDPartido INT NOT NULL,
 	CorreoUsuario VARCHAR(50) NOT NULL,
-	ContraseñaUsuario VARCHAR(128) NOT NULL,
 	Resultado CHAR(1) NULL,
 	[Over/Under] DECIMAL(2,1) NULL,
 	GolesLocal TINYINT NULL,
@@ -64,7 +66,13 @@ CREATE TABLE BET_Apuestas (
 	
 	CONSTRAINT PKApuestas PRIMARY KEY(ID),
 	CONSTRAINT FKApuestasPartidos FOREIGN KEY(IDPartido) REFERENCES BET_Partidos(ID) ON DELETE CASCADE ON UPDATE CASCADE,
-	--CONSTRAINT FKApuestasUsuarios FOREIGN KEY(CorreoUsuario) REFERENCES BET_Usuarios(CorreoElectronico) ON DELETE NO ACTION ON UPDATE CASCADE
+	CONSTRAINT FKApuestasUsuarios FOREIGN KEY(CorreoUsuario) REFERENCES BET_Usuarios(CorreoElectronico) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT CKCuota CHECK(Cuota > 1.00),
+	CONSTRAINT CKGolesLocalApuesta CHECK (GolesLocal > 0),
+	CONSTRAINT CKGolesVisitanteApuesta CHECK (GolesVisitante > 0),
+	CONSTRAINT CKTipo CHECK (Tipo IN ('GanadorPartido', 'Over/under', 'ResultadoExacto')),
+	CONSTRAINT CKResultado CHECK (Resultado IN ('1', 'x', '2')),
+	CONSTRAINT CKOverUnder CHECK ([Over/Under] % 0.5 = 0)
 	 
 )
 GO
@@ -73,12 +81,11 @@ CREATE TABLE BET_Movimientos (
 
 	ID INT NOT NULL IDENTITY(1,1),
 	CorreoUsuario VARCHAR(50) NOT NULL,
-	ContraseñaUsuario VARCHAR(128) NOT NULL,
 	Cantidad SMALLMONEY NOT NULL,
 	IDApuesta INT NULL,
 
 	CONSTRAINT PKMovimientos PRIMARY KEY(ID),
-	--CONSTRAINT FKMovimientosUsuarios FOREIGN KEY(CorreoUsuario) REFERENCES BET_Usuarios(CorreoElectronico) 
-	CONSTRAINT FKMovimientosApuestas FOREIGN KEY(IDApuesta) REFERENCES BET_Apuestas(ID) ON DELETE NO ACTION ON UPDATE CASCADE
+	CONSTRAINT FKMovimientosUsuarios FOREIGN KEY(CorreoUsuario) REFERENCES BET_Usuarios(CorreoElectronico) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT FKMovimientosApuestas FOREIGN KEY(IDApuesta) REFERENCES BET_Apuestas(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 
 )
