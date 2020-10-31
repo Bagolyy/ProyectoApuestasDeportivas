5ab59e8baaab95ba7afe6a19d05aad33d0ce10ea
Alex1sK1ng <alexgatabarros@gmail.com>
2020-10-31 20:36:55 +0100

    Triggers

---
+++ Triggers.sql
@@ -0,0 +1,18 @@
+USE BETi
+GO
+
+--Usamos excepciones
+EXECUTE sys.sp_addmessage @msgnum = 55000, @severity = 16, @msgtext = N'
+You can not modify or delete a registered bet', @lang = 'us_english', @replace = 'replace';
+EXECUTE sys.sp_addmessage @msgnum = 55000, @severity = 16, @msgtext = N'No se puede modificar ni eliminar una apuesta ya realizada', @lang = 'spanish', @replace = 'replace';
+GO
+
+--Trigger que controla que no se puedan modificar ni eliminar apuestas ya realizadas.
+CREATE TRIGGER ModificarApuesta ON BET_Apuestas INSTEAD OF UPDATE, DELETE AS
+	BEGIN
+
+		DECLARE @Message NVARCHAR(255) = FormatMessage(55000);
+		THROW 55000, @Message, 1
+
+	END
+GO
