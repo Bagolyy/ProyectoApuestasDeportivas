1c660c2dbf8ee21579885cbfe6d0d50e51354403
Alex1sK1ng <alexgatabarros@gmail.com>
2020-11-05 20:57:21 +0100

    Correcciones

--- Triggers.sql
+++ Triggers.sql
@@ -1,7 +1,4 @@
 
---Comprobar premio supera máximo fijado.
-
-
 USE BETi
 GO
 
@@ -12,7 +9,6 @@ You can not modify or delete a registered bet', @lang = 'us_english', @replace =
 EXECUTE sys.sp_addmessage @msgnum = 55000, @severity = 16, @msgtext = N'No se puede modificar ni eliminar una apuesta ya realizada', @lang = 'spanish', @replace = 'replace';
 GO
 
---Usamos excepciones
 EXECUTE sys.sp_addmessage @msgnum = 56000, @severity = 16, @msgtext = N'
 You do not have enough money', @lang = 'us_english', @replace = 'replace';
 EXECUTE sys.sp_addmessage @msgnum = 56000, @severity = 16, @msgtext = N'No posee saldo suficiente', @lang = 'spanish', @replace = 'replace';
@@ -22,7 +18,10 @@ EXECUTE sys.sp_addmessage @msgnum = 57000, @severity = 16, @msgtext = N'The matc
 EXECUTE sys.sp_addmessage @msgnum = 57000, @severity = 16, @msgtext = N'El partido no se encuentra en el rango de tiempo apostable', @lang = 'spanish', @replace = 'replace';
 GO
 
---FUNCIONES
+EXECUTE sys.sp_addmessage @msgnum = 58000, @severity = 16, @msgtext = N'
+This bet exceeds the maximun allowed. Your bet could not be placed ', @lang = 'us_english', @replace = 'replace';
+EXECUTE sys.sp_addmessage @msgnum = 58000, @severity = 16, @msgtext = N'Esta apuesta supera el máximo permitido. No se ha podido realizar su apuesta', @lang = 'spanish', @replace = 'replace';
+GO
 
 
 --PROCEDIMIENTOS ALMACENADOS
@@ -70,6 +69,9 @@ CREATE PROCEDURE PoblarEquipos AS
 
 GO
 
+EXECUTE PoblarEquipos
+GO
+
 --Estudio Interfaz
 --	Propósito: generar una cuota que será aplicada a una apuesta determinada.
 --	Signtaura: CREATE FUNCTION GenerarCuota (@Tipo TINYINT, @IDPartido INT, @Resultado CHAR(1) = NULL, @Over_under DECIMAL(4,2) = NULL, @GolesLocal_Visitante DECIMAL(2,1) = NULL)
@@ -90,6 +92,9 @@ CREATE PROCEDURE GenerarCuota @Tipo TINYINT, @IDPartido INT, @Resultado CHAR(1) 
 	
 GO	 
 
+DECLARE @Cuota DECIMAL(4,2)
+EXECUTE GenerarCuota 1, 1, '1', NULL, NULL, @Cuota OUTPUT
+GO
 
 --Estudio Interfaz
 --	Propósito: registrar una apuesta de un determinado tipo, realizada por un usuario, a un determinado partido.
@@ -101,7 +106,7 @@ GO
 --	Salidas: ninguna.	
 --	Postcondición: se graba en la tabla apuesta la correspondiente apuesta con los datos especificados.
 
-CREATE PROCEDURE RealizarApuesta @TipoApuesta TINYINT, @CantidadApostada SMALLMONEY, @IDPartido INT, @CorreoUsuario VARCHAR(50), @Resultado CHAR(1) = NULL, @Over_under DECIMAL(4,2) = NULL, @GolesLocal_Visitante DECIMAL(2,1) = NULL AS
+ALTER PROCEDURE RealizarApuesta @TipoApuesta TINYINT, @CantidadApostada SMALLMONEY, @IDPartido INT, @CorreoUsuario VARCHAR(50), @Resultado CHAR(1) = NULL, @Over_under DECIMAL(4,2) = NULL, @GolesLocal_Visitante DECIMAL(2,1) = NULL AS
 
 	BEGIN
 
@@ -126,20 +131,34 @@ CREATE PROCEDURE RealizarApuesta @TipoApuesta TINYINT, @CantidadApostada SMALLMO
 			BEGIN
 	
 				--Generamos una cuota aleatoria
-				EXECUTE GenerarCuota @Tipo, @IDPartido, @Resultado, @Over_under, @GolesLocal_Visitante, @Cuota OUTPUT
+				EXECUTE GenerarCuota @TipoApuesta, @IDPartido, @Resultado, @Over_under, @GolesLocal_Visitante, @Cuota OUTPUT
 
-				--Insertamos la nueva apuesta
-				INSERT INTO BET_Apuestas VALUES(@TipoApuesta, @Tipo, @Cuota, @CantidadApostada, @IDPartido, @CorreoUsuario, @Resultado, @Over_under, @GolesLocal_Visitante)
+				IF([dbo].ComprobarSuperaMaximo(@IDPartido, @Cuota, @TipoApuesta, @CantidadApostada, @Resultado, @Over_under, @GolesLocal_Visitante) = 1)
 
-				SET @IDApuesta = (SELECT ID FROM BET_Apuestas WHERE ID = @@IDENTITY)
+					BEGIN
 
-				--Actualizamos el saldo del usuario
-				UPDATE BET_Usuarios
-				SET Saldo = Saldo - @CantidadApostada
-				WHERE CorreoElectronico = @CorreoUsuario
+						DECLARE @MessageSupera NVARCHAR(255) = FormatMessage(58000);
+						THROW 58000, @MessageSupera, 1
 
-				--Insertamos el nuevo movimiento
-				INSERT INTO BET_Movimientos VALUES (@CorreoUsuario, @CantidadApostada, @IDApuesta)
+					END
+
+				ELSE
+					BEGIN
+
+						--Insertamos la nueva apuesta
+						INSERT INTO BET_Apuestas VALUES(@Tipo, @Cuota, @CantidadApostada, 0, @IDPartido, @CorreoUsuario, @Resultado, @Over_under, @GolesLocal_Visitante)
+
+						SET @IDApuesta = (SELECT ID FROM BET_Apuestas WHERE ID = @@IDENTITY)
+
+						--Actualizamos el saldo del usuario
+						UPDATE BET_Usuarios
+						SET Saldo = Saldo - @CantidadApostada
+						WHERE CorreoElectronico = @CorreoUsuario
+
+						--Insertamos el nuevo movimiento
+						INSERT INTO BET_Movimientos VALUES (@CorreoUsuario, @CantidadApostada, @IDApuesta)
+
+					END
 
 			END
 
@@ -147,14 +166,18 @@ CREATE PROCEDURE RealizarApuesta @TipoApuesta TINYINT, @CantidadApostada SMALLMO
 			
 			BEGIN
 
-				DECLARE @Message NVARCHAR(255) = FormatMessage(56000);
-				THROW 56000, @Message, 1
+				DECLARE @MessageSaldo NVARCHAR(255) = FormatMessage(56000);
+				THROW 56000, @MessageSaldo, 1
 
 			END
 
 	END
 
 GO
+
+EXECUTE RealizarApuesta 1, 80.0, 1, 'zamit@gmail.com', '1', NULL, NULL
+GO
+
 
 --Estudio Interfaz
 --	Propósito: grabar en la tabla partidos todas las combinaciones posibles entre los equipos que participan en la competición. Se establecerán todos los partidos como no finalizados para que se puedan realizar las apuestas.
@@ -167,8 +190,8 @@ CREATE PROCEDURE PoblarPartidos AS
 
 	BEGIN
 
-		INSERT INTO BET_Partidos (IDEquipoLocal, IDEquipoVisitante)
-		SELECT L.ID, V.ID FROM BET_Equipos AS L CROSS JOIN BET_Equipos AS V WHERE L.ID != V.ID
+		INSERT INTO BET_Partidos (IDEquipoLocal, IDEquipoVisitante, GolesLocal, GolesVisitante, Apostable, Finalizado)
+		SELECT L.ID, V.ID, 0, 0, 0, 0 FROM BET_Equipos AS L CROSS JOIN BET_Equipos AS V WHERE L.ID != V.ID
 
 		DECLARE CPartidos CURSOR FOR SELECT ID FROM BET_Partidos
 		DECLARE @Partido SMALLINT
@@ -189,7 +212,7 @@ CREATE PROCEDURE PoblarPartidos AS
 				SET @FechaPartido = DATEADD(DAY, @DiasAux, CAST(GETDATE() AS SMALLDATETIME))
 
 				UPDATE BET_Partidos
-				SET GolesLocal = 0, GolesVisitante = 0, Apostable = 0, Finalizado = 0, Fecha = @FechaPartido
+				SET Fecha = @FechaPartido
 				WHERE @Partido = ID
 
 				FETCH NEXT FROM CPartidos INTO @Partido
@@ -201,6 +224,9 @@ CREATE PROCEDURE PoblarPartidos AS
 
 	END
 
+GO
+
+EXECUTE PoblarPartidos
 GO
 
 --Estudio Interfaz
@@ -259,6 +285,12 @@ CREATE PROCEDURE IngresarDinero @CorreoUsuario VARCHAR(50), @CantidadIngresar MO
 
 GO
 
+EXECUTE IngresarDinero 'zamit@gmail.com', 100.0
+GO
+
+SELECT * FROM BET_Usuarios
+GO
+
 --Estudio Interfaz
 --	Propósito: actualizar el saldo del usuario tras realizar una retirada de saldo.
 --	Signatura: CREATE PROCEDURE RetirarDinero @CorreoUsuario VARCHAR(50), @CantidadRetirar MONEY
@@ -266,23 +298,26 @@ GO
 --	Salidas: ninguna.
 --	Postcondiciones: se actualiza el saldo del usuario.
 
-CREATE PROCEDURE RetirarDinero @CorreoUsuario VARCHAR(50), @CantidadRetirar MONEY AS
+ALTER PROCEDURE RetirarDinero @CorreoUsuario VARCHAR(50), @CantidadRetirar MONEY AS
 
 	BEGIN
-
-		UPDATE BET_Usuarios 
-		SET Saldo = Saldo - @CantidadRetirar
-		WHERE CorreoElectronico = @CorreoUsuario
 
 		DECLARE @SaldoUsuario MONEY
 		SET @SaldoUsuario = (SELECT Saldo FROM BET_Usuarios WHERE CorreoElectronico = @CorreoUsuario)
 
-		IF( @SaldoUsuario < 0.0)
+		IF(@SaldoUsuario - @CantidadRetirar < 0.0)
 			BEGIN
 				UPDATE BET_Usuarios 
 				SET Saldo = 0
 				WHERE CorreoElectronico = @CorreoUsuario
 			END
+		ELSE 
+			BEGIN
+				UPDATE BET_Usuarios 
+				SET Saldo = Saldo - @CantidadRetirar
+				WHERE CorreoElectronico = @CorreoUsuario
+			END
+
 
 		INSERT INTO BET_Movimientos
 		VALUES(@CorreoUsuario, @CantidadRetirar, NULL)
@@ -291,15 +326,49 @@ CREATE PROCEDURE RetirarDinero @CorreoUsuario VARCHAR(50), @CantidadRetirar MONE
 
 GO
 
+EXECUTE RetirarDinero 'zamit@gmail.com', 10.0
+GO
 
 --FUNCIONES
 
 --Estudio Interfaz
---	Propósito: comprobar si los premios de una 
---	
+--	Propósito: comprobar si los premios de una misma apuesta a un determinado evento supera el máximo fijado. 
+--	Signatura: CREATE FUNCTION ComprobarSuperaMaximo (@IDPartido INT, @Cuota DECIMAL(4,2), @Tipo TINYINT, @CantidadApostada SMALLMONEY, @Resultado CHAR(1) = NULL, @Over_under DECIMAL(4,2) = NULL, @GolesLocal_Visitante DECIMAL(2,1) = NULL) RETURNS BIT
+--	Entradas: el ID del partido, el tipo de apuesta, la cuota y cantidad apostada para calcular el beneficio y, el tipo de apuesta que se realiza.
+--	Salidas: un valor booleano que indique que si supera el máximo o no.
+--	Postcondiciones: se devuelve el valor de tipo BIT asociado al nombre.
+--		0 --> No supera
+--		1 --> Supera
 
-CREATE FUNCTION ComprobarSuperaMaximo
+CREATE FUNCTION ComprobarSuperaMaximo (@IDPartido INT, @Cuota DECIMAL(4,2), @Tipo TINYINT, @CantidadApostada SMALLMONEY, @Resultado CHAR(1) = NULL, @Over_under DECIMAL(4,2) = NULL, @GolesLocal_Visitante DECIMAL(2,1) = NULL) RETURNS BIT AS
 
+	BEGIN
+		
+		DECLARE @Supera BIT
+		SET @Supera = 0
+		DECLARE @BeneficioTotal MONEY
+			
+		SET @BeneficioTotal	= (CASE @Tipo
+
+									WHEN 1 THEN (SELECT SUM(CantidadApostada * Cuota) FROM BET_Apuestas WHERE IDPartido = @IDPartido AND Resultado = @Resultado)
+									WHEN 2 THEN (SELECT SUM(CantidadApostada * Cuota) FROM BET_Apuestas WHERE IDPartido = @IDPartido AND [Over/under] = @Over_under)
+									WHEN 3 THEN (SELECT SUM(CantidadApostada * Cuota) FROM BET_Apuestas WHERE IDPartido = @IDPartido AND GolesLocal_Visitante = @GolesLocal_Visitante)
+
+								END)
+
+
+		IF(@BeneficioTotal + (@Cuota*@CantidadApostada) > 10000.0)
+
+			BEGIN
+				SET @Supera = 1
+			END
+	
+		RETURN @Supera
+
+	END
+
+
+GO
 
 --TRIGGERS
 
@@ -332,4 +401,15 @@ CREATE TRIGGER ComprobarPartidoApostable ON BET_Apuestas INSTEAD OF INSERT AS
 			END
 
 	END
+GO
+
+--Trigger que muestra un mensaje de confirmación de que se ha realizado la apuesta.
+CREATE TRIGGER ConfirmacionApueta ON BET_Apuestas AFTER INSERT AS
+	
+	BEGIN
+
+		PRINT 'La apuesta se ha realizado correctamente'
+
+	END
+
 GO
